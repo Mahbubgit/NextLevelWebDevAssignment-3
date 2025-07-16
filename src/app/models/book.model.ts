990e5c8143758a0f7bed1f11fb1dd457d3528d01
Your Name <you@example.com>
2025-07-16 09:26:17 +0600

    add book.interface and book.model

---
+++ src/app/models/book.model.ts
@@ -0,0 +1,41 @@
+
+import { Schema } from "mongoose";
+import { IBook } from "../interfaces/book.interface";
+
+const bookSchema = new Schema<IBook>({
+    title: {
+        type: String,
+        required: [true, "The book's title can not be null!"],
+        trim: true
+    },
+    author: {
+        type: String,
+        required: [true, "The book's author can not be null!"],
+        trim: true
+    },
+    genre: {
+        type: String,
+        uppercase: true,
+        enum: {
+            values: ['FICTION', 'NON_FICTION', 'SCIENCE', 'HISTORY', 'BIOGRAPHY', 'FANTASY'],
+            message: "{VALUE} is not a valid genre. Please provide a valid genre."
+        }
+    },
+    isbn: {
+        type: String,
+        required :[true, "ISBN can not be null!"],
+        unique: [true, "{VALUE} is already exist. Please provide another ISBN."],
+    },
+    description: {
+        type: String,
+    },
+    copies: {
+        type: Number,
+        required: [true, "Copies can not be null!"],
+        min: [1, "Copies must be greater than or equal to 1. Invalid Copies: {VALUE}"]
+    },
+    available: {
+        type: Boolean,
+        default: true
+    }
+})
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import {Server} from 'http';
+import app from './app';
+import mongoose from 'mongoose';
+
+let server: Server;
+
+const PORT = 5000;
+async function main() {
+try {
+    await mongoose.connect('mongodb+srv://mongodb:mongodb@cluster0.izigbyf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+    console.log("Connected to MongoDB using Mongoose!!");
+    server = app.listen(PORT, () =>{
+        console.log(`App is listening on port ${PORT}`);
+    });
+} catch (error) {
+    console.log(error);
+}    
+}
+
+main()
