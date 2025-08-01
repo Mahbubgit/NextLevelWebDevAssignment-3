b68a4068c84dbaad90a1cb185cbe7f9e21e1f068
Your Name <you@example.com>
2025-08-01 11:58:28 +0600

    Borrowed Books Summary without match

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { Borrows } from "../models/borrow.model";
 import { Books } from "../models/book.model";
+import mongoose from "mongoose";
 
 export const borrowRoutes = express.Router();
 
@@ -53,3 +54,90 @@ borrowRoutes.post('/', async (req: Request, res: Response) => {
     }
 
 });
+
+// Borrowed Books Summary (Using Aggregation)
+
+borrowRoutes.get('/', async (req: Request, res: Response) => {
+    try {
+        let borrowedBooks = [];
+
+        borrowedBooks = await Borrows.aggregate([
+
+            {
+                $facet: {
+                    //pipeline-1
+                    "book": [
+                        {
+                            $group: {
+                                _id: "$book",
+                            }
+                        },
+                    ],
+                    //pipeline-2
+                    "totalQuantity": [
+                        {
+                            $group: {
+                                _id: "$book",
+                                totalQuantity: { $sum: "$quantity" }
+                            }
+                        },
+                    ],
+                }
+            },
+            {
+                $lookup: {
+                    from: "books",
+                    localField: "_id",
+                    foreignField: "book",
+                    as: "book"
+                }
+            },
+
+            {
+                $unwind: "$book",
+            },
+            {
+                $unwind: "$totalQuantity",
+            },
+            // {
+            //     $match: {
+            //         "totalQuantity._id": { $ne: ["book._id"] }, 
+            //     }
+            // },
+
+            // {
+            //     $match: {
+            //         "book._id": new mongoose.Types.ObjectId("6884d9762626d2659ed3feee"),
+            //6884d9762626d2659ed3feee, 6884d99d2626d2659ed3fef0, 6884d9c72626d2659ed3fef2
+            //         // "totalQuantity.totalQuantity": 5,
+            //         // "book.title": "The Theory of Everything",
+            //         // "book._id" : "totalQuantity._id"
+            //     }
+            // },
+            {
+                $project: {
+                    _id: 0,
+                    // "book._id": 1,
+                    "book.title": 1,
+                    "book.isbn": 1,
+                    // "book.copies": 1,
+                    // "totalQuantity._id": 1,
+                    // "totalQuantity.totalQuantity": 1,
+                    "totalQuantity": "$totalQuantity.totalQuantity",
+                },
+            },
+        ]);
+
+        res.status(201).json({
+            success: true,
+            message: "Borrowed books summary retrieved successfully",
+            data: borrowedBooks
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: "Failed to retrieve borrowed books summary.",
+        });
+    }
+})
